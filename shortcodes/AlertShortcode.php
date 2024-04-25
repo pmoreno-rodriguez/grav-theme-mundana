bd897f1cb66f27cd5bfdfa6a5c9c88d67831d158
pmoreno.rodriguez <pmoreno.rodriguez@outlook.com>
2024-04-25 22:24:14 +0200

    Redesigned Bootstrap Shortcodes

--- shortcodes/AlertShortcode.php
+++ shortcodes/AlertShortcode.php
@@ -7,43 +7,14 @@ class AlertShortcode extends Shortcode
 {
     public function init()
     {
-        $this->shortcode->getHandlers()->add('sc-alert', function(ShortcodeInterface $sc) {
+        $this->shortcode->getHandlers()->add('sc-alert', function (ShortcodeInterface $sc) {
+            // Render the Twig template with shortcode parameters
+            $output = $this->grav['twig']->processTemplate(
+                'partials/shortcodes/alert.html.twig',
+                ['shortcode' => $sc]
+            );
 
-            // Get shortcode content and parameters
-            $alertClass = $sc->getParameter('class', 'primary');
-            $alertDismissible = $sc->getParameter('dismissible', 'false');
-            $alertHeading = $sc->getParameter('heading');
-            $alertText = $sc->getContent();
-
-            if (empty($alertClass)) {
-              $alertClass = "primary";
-            }
-
-            $closeButton = '';
-            $classDismissible = '';
-
-            if ($alertDismissible === 'true') {
-                $classDismissible = 'alert-dismissible fade show';
-                $closeButton = '
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>';
-            }
-
-            if (!empty($alertHeading)) {
-                $output = '<div class="alert '.$classDismissible .' alert-'.$alertClass.'" role="alert">
-                            <h4 class="alert-heading">'.$alertHeading.'</h4>
-                            '.$alertText.$closeButton.'
-                            </div>';
-                return $output;
-            }
-            else {
-                $output = '<div class="alert '.$classDismissible .' alert-'.$alertClass.'" role="alert">
-                            '.$alertText.$closeButton.'
-                            </div>';
-                return $output;
-            }
-
+            return $output;
         });
     }
 }
